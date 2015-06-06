0c622e9a40e429dcf72629ac5a4e61680f5b9183
F1xik <9f1xik@gmail.com>
2015-06-06 04:29:41 +0300

    non-optimized
    v 0.1

---
+++ oop5/OOPlaba4/MainWindow.xaml.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Xml.Serialization;
+using Weapons;
+using TegForExtensions;
+using Interfaces;
+using AboutInterfaceComparers;
+
+
+namespace OOPlaba4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string pluginPath = System.IO.Path.Combine(
+                                                Directory.GetCurrentDirectory(),
+                                                "Plugins");
+        static JsonSerializer jsonSerializer = new JsonSerializer();
+        List<string> classesNames = new List<string>();
+        List<Type> dllTypes = new List<Type>();
+        List<Type> otherDllTypes = new List<Type>();
+        List<IExt> plugins = new List<IExt>();
+        List<IComparer> comparerPlugins = new List<IComparer>();
+        string sourcePath = Environment.CurrentDirectory;
+        ObservableCollection<Weapon> weapons = new ObservableCollection<Weapon>();
+        ObservableCollection<Weapon> weapons1 = new ObservableCollection<Weapon>();
+        ObservableCollection<Weapon> weapons2 = new ObservableCollection<Weapon>();
+        ObservableCollection<TegForExtensions.Extension> extensions = new ObservableCollection<TegForExtensions.Extension>();
+        
+        public MainWindow()
+        {   
+            InitializeComponent();
+        }
+
+        class Wrapper
+        {
+            public ObservableCollection<Weapon> Weapons { get; set; }
+        }
+
+        private void RefreshPlugins()
+        {
+            plugins.Clear();
+            //MessageBox.Show(pluginPath);
+
+            DirectoryInfo pluginDirectory = new DirectoryInfo(pluginPath);
+            if (!pluginDirectory.Exists)
+                pluginDirectory.Create();                 
+            var pluginFiles = Directory.GetFiles(pluginPath, "*.dll");
+            foreach (var file in pluginFiles)
+            {
+                
+                Assembly asm = Assembly.LoadFrom(file);                
+                var types = asm.GetTypes().
+                                Where(t => t.GetInterfaces().
+                                Where(i => i.FullName == typeof(IExt).FullName).Any());
+                var typesChkCompare = asm.GetTypes().
+                                Where(t => t.GetInterfaces().
+                                Where(i => i.FullName == typeof(IComparer).FullName).Any());
+                
+                foreach (var type in types)
+                {
+                    var plugin = asm.CreateInstance(type.FullName) as IExt;
+                    plugins.Add(plugin);
+                }
+                foreach (var type in typesChkCompare)
+                {
+                    var plugin = asm.CreateInstance(type.FullName) as IComparer;
+                    comparerPlugins.Add(plugin);
+                }
+            }
+        }
+        private void ActivateSaveCRCPlugins()
+        {
+            foreach (var plugin in plugins)
+                plugin.saveChkSum(weapons, "test.txt");
+            
+        }
+        private void ActivateCompareCRCPlugins()
+        {
+            
+            foreach (var plugin in comparerPlugins)
+                MessageBox.Show("CRC is Correct? " + plugin.ChkSumCompare("test.txt").ToString());
+        }
+
+        private void CreateFilesPaths()
+        {
+            List<string> filesPaths = new List<string>(Directory.EnumerateFiles(sourcePath));
+            for (int i = 0; i < filesPaths.Count; i++)
+            {
+                if (System.IO.Path.GetExtension(filesPaths[i]) == ".dll")
+                {
+                    classesNames.Add(filesPaths[i]);
+                }
+            }
+        }
+
+        private void CreateAllTypes()
+        {
+            for (int i = 0; i < classesNames.Count; i++)
+            {
+                Assembly temp = Assembly.LoadFrom(classesNames[i]);
+                dllTypes.AddRange(temp.GetExportedTypes().Where(type => type.IsSubclassOf(typeof(Weapon))));
+                otherDllTypes.AddRange(temp.GetExportedTypes().Where(type => type.IsSubclassOf(typeof(TegForExtensions.Extension))));
+                //MessageBox.Show(otherDllTypes[i].ToString());
+            }
+            
+        }
+
+        private void CreateAllObjects()
+        {
+            object newWeapon;
+            for (int i = 0; i < dllTypes.Count; i++)
+            {
+                newWeapon = Activator.CreateInstance(dllTypes[i]);
+                weapons1.Add((Weapon)newWeapon);
+            }
+            newWeapon = new Weapon();
+            weapons1.Add((Weapon)newWeapon);
+            object newExt;
+            for (int i = 0; i < otherDllTypes.Count; i++)
+            {
+
+                newExt = Activator.CreateInstance(otherDllTypes[i]) as IExt;
+                
+                extensions.Add((TegForExtensions.Extension)newExt);
+            }
+            
+        }
+
+        
+
+        private void ShowAllProperties(ListBox list)
+        {
+            int i = 0;
+            Type type = list.SelectedItem.GetType();
+            Fields.Children.Clear();
+            foreach (PropertyInfo property in type.GetProperties())
+            {
+                TextBox textBox = new TextBox();
+                Label label = new Label();
+                Binding binding = new Binding();
+                binding.Source = list.SelectedItem;
+                binding.Path = new PropertyPath(property.Name);
+                binding.Mode = BindingMode.TwoWay;
+                
+                label.Content = property.Name;
+                textBox.SetBinding(TextBox.TextProperty, binding);
+                Grid.SetColumn(label, 0);
+                Grid.SetColumn(textBox, 1);
+                Grid.SetRow(label, i);
+                Grid.SetRow(textBox, i);
+
+                var rowDefinition = new RowDefinition { Height = GridLength.Auto };
+                Fields.RowDefinitions.Add(rowDefinition);
+                Fields.Children.Add(textBox);
+                Fields.Children.Add(label);
+                i++;
+            }
+        }
+
+        private void GetListOfWeapons_Click(object sender, RoutedEventArgs e)
+        {
+            CreateFilesPaths();
+            CreateAllTypes();
+            CreateAllObjects();
+            RefreshPlugins();           
+            WeaponsTypes.ItemsSource = weapons1;
+            WeaponsList.ItemsSource = weapons;
+            listOfExtensions.ItemsSource = extensions;
+        }
+
+
+        
+
+        private void Serialization_Click(object sender, RoutedEventArgs e)
+        {
+            File.Delete("test.txt");
+            ActivateSaveCRCPlugins();
+            FileStream stream;
+            stream = File.Open("BsonFile", FileMode.OpenOrCreate, FileAccess.Write);
+
+            using (BsonWriter bsonWriter = new BsonWriter(stream))
+            {
+                jsonSerializer.TypeNameHandling = TypeNameHandling.All;
+                var obj = new Wrapper { Weapons = weapons };                
+                jsonSerializer.Serialize(bsonWriter, obj);
+            }
+            stream.Close();
+            
+
+        }
+
+        private void Deserialization_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream stream;
+            stream = File.Open("BsonFile", FileMode.Open, FileAccess.Read);
+
+            using (BsonReader bsonReader = new BsonReader(stream))
+            {
+                jsonSerializer.TypeNameHandling = TypeNameHandling.All;
+                var obj = new Wrapper { Weapons = weapons2 };
+                
+                obj = jsonSerializer.Deserialize<Wrapper>(bsonReader);
+                
+                weapons2 = obj.Weapons;
+                DsrlzList.ItemsSource = weapons2;
+                
+
+            }
+            stream.Close();
+            ActivateSaveCRCPlugins();
+            ActivateCompareCRCPlugins();
+            
+        }
+
+        private void AddToHollowsList_Click(object sender, RoutedEventArgs e)
+        {
+            Type weaponType = WeaponsTypes.SelectedItem.GetType();
+            object newWeapons = Activator.CreateInstance(weaponType);            
+            weapons.Add((Weapon)newWeapons);
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if(WeaponsList.SelectedValue != null)
+                ShowAllProperties(WeaponsList);
+            else
+            {
+                ShowAllProperties(DsrlzList);
+            }
+        }
+    }
+}
